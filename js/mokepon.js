fa5d332712fd59abfd6524fc493cf7d25c79760b
lucazfrZ <lucasdev2503@gmail.com>
2023-12-02 16:46:00 -0300

    Opmtimizar código [DRY]

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,43 +1,59 @@
-let ataqueJugador /*Variable global*/
+/*Inicio Variables de Función "iniciarJuego"*/
+const sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+const sectionReiniciar = document.getElementById('boton-Reiniciar')
+const botonMascotaJugador = document.getElementById('boton-mascota')
+const botonFuego = document.getElementById('boton-fuego')
+const botonAgua = document.getElementById('boton-agua')
+const botonTierra = document.getElementById('boton-tierra')
+const botonReiniciar = document.getElementById('boton-Reiniciar')
+/*Fin Variables de Función "iniciarJuego"*/
+
+/*Inicio Variables de Función "seleccionarMascotaJugador"*/
+const sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+// let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+const inputHipodege = document.getElementById('hipodoge')
+const inputCapipepo = document.getElementById('capipepo')
+const inputRatigueya = document.getElementById('ratigueya')
+const spanMascotaJugador = document.getElementById('mascota-Jugador')
+/*Fin Variables de Función "seleccionarMascotaJugador"*/
+
+/*Inicio Variables de Función "seleccionarMascotaOponente"*/
+const spanMascotaOponente = document.getElementById('mascota-Oponente')
+/*Fin Variables de Función "seleccionarMascotaOponente"*/
+
+/*Inicio Variables de Función "combate"*/
+const spanVidaJugador = document.getElementById('vida-Jugador')
+const spanVidaOponente = document.getElementById('vida-Oponente')
+/*Fin Variables de Función "combate"*/
+
+/*Inicio Variables de Funcion "crearMensaje"*/
+const sectionMensaje = document.getElementById('resultado')
+const ataqueDelJugador = document.getElementById('ataques-del-jugador')
+const ataqueDelOponente = document.getElementById('ataques-del-oponente')
+/*Fin Variables de Funcion "crearMensaje"*/
+
+/*Inicio Variables Globales*/
+let ataqueJugador 
 let ataqueOponente
 let vidaJugador = 3
 let vidaOponente = 3
+/*Fin Variables Globales*/
 
 function iniciarJuego(){
-    
-    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
     sectionSeleccionarAtaque.style.display = 'none'
-
-    let sectionReiniciar = document.getElementById('boton-Reiniciar')
     sectionReiniciar.style.display='none'
 
-    let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
-
-    let botonFuego = document.getElementById('boton-fuego')
     botonFuego.addEventListener('click', ataqueFuego)
-    let botonAgua = document.getElementById('boton-agua')
     botonAgua.addEventListener('click', ataqueAgua)
-    let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
-
-    let botonReiniciar = document.getElementById('boton-Reiniciar')
     botonReiniciar.addEventListener('click', reiniciarJuego)
-    
 }
 
 /*Esta función permite seleccionar la mascota del jugador */ 
 function seleccionarMascotaJugador(){
-    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
-    sectionSeleccionarMascota.style.display = 'none'
-
-    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarMascota.style.display = 'none'    
     sectionSeleccionarAtaque.style.display = 'flex'
-
-    let inputHipodege = document.getElementById('hipodoge')
-    let inputCapipepo = document.getElementById('capipepo')
-    let inputRatigueya = document.getElementById('ratigueya')
-    let spanMascotaJugador = document.getElementById('mascota-Jugador')
 
     if(inputHipodege.checked){
         spanMascotaJugador.innerHTML = "Hipodoge"/*innerHTML es una propiedad que permite manipular el DOM */
@@ -50,16 +66,14 @@ function seleccionarMascotaJugador(){
     }
     else{
         alert("Hola querido usuario, selecciona una mascota")
-        reiniciarJuego()
+        location.reload()
     }
-
     seleccionarMascotaOponente()
 }
 
 function seleccionarMascotaOponente(){
     let mascotaAleatorio = aleatorio(1,3)
-    let spanMascotaOponente = document.getElementById('mascota-Oponente')
-
+    
     if(mascotaAleatorio == 1){
         spanMascotaOponente.innerHTML = "Hipodoge"
     }
@@ -100,12 +114,6 @@ function ataqueAleatorioOponente(){
 }
 
 function combate(){
-    
-
-    let spanVidaJugador = document.getElementById('vida-Jugador')
-    let spanVidaOponente = document.getElementById('vida-Oponente')
-
-
     if(ataqueOponente == ataqueJugador){
         crearMensaje("EMPATE😐🤨")
     }
@@ -129,7 +137,6 @@ function combate(){
     vidaJugador--
     spanVidaJugador.innerHTML = vidaJugador
     }
-
     revisarVida()
 }
 
@@ -143,40 +150,33 @@ function revisarVida(){
 }
 
 function crearMensaje(resultado){
-    let sectionMensaje = document.getElementById('resultado')
-    let ataqueDelJugador = document.getElementById('ataques-del-jugador')
-    let ataqueDelOponente = document.getElementById('ataques-del-oponente')
     /*Podre declarar variables pero nunca podre declarar mi amor a ella */
-
     let nuevoAtaqueDelJugador = document.createElement('p')
     let nuevoAtaqueDelOponente = document.createElement('p')
 
     sectionMensaje.innerHTML = resultado
     nuevoAtaqueDelJugador.innerHTML = ataqueJugador
     nuevoAtaqueDelOponente.innerHTML = ataqueOponente
-
-    // let parrafo = document.createElement('p') /* Este método permite crear elementos com por ejemplp <p> en el HTML*/
-    // parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + ", la mascota de tu oponente atacó con " + ataqueOponente + " - " + resultado
-
-   /*Si el hijo(Child) es una referencia(hace referencia) hacia un nodo existente en el documento actual, este es quitado del padre actual para ser puesto en el nodo padre nuevo. La clave está en si el (Child) es una referencia a un nodo existente en el documento. */
-   
+// let parrafo = document.createElement('p') /* Este método permite crear elementos com por ejemplp <p> en el HTML*/
+ // parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + ", la mascota de tu oponente atacó con " + ataqueOponente + " - " + resultado
+ /*Si el hijo(Child) es una referencia(hace referencia) hacia un nodo existente en el documento actual, este es quitado del padre actual para ser puesto en el nodo padre nuevo. La clave está en si el (Child) es una referencia a un nodo existente en el documento. */
     ataqueDelJugador.appendChild(nuevoAtaqueDelJugador)
     ataqueDelOponente.appendChild(nuevoAtaqueDelOponente)
 }
 
 function crearMensajeFinal(resultadoFinal){
-    let sectionMensaje = document.getElementById('resultado')
+    
 
      /* Este método permite crear elementos com por ejemplp <p> en el HTML*/
     sectionMensaje.innerHTML = resultadoFinal
 
     /*Si el hijo(Child) es una referencia(hace referencia) hacia un nodo existente en el documento actual, este es quitado del padre actual para ser puesto en el nodo padre nuevo. La clave está en si el (Child) es una referencia a un nodo existente en el documento. */
 
-   let botonFuego = document.getElementById('boton-fuego')
+   
     botonFuego.disabled = true
-    let botonAgua = document.getElementById('boton-agua')
+    
     botonAgua.disabled = true
-    let botonTierra = document.getElementById('boton-tierra')
+    
     botonTierra.disabled = true
 
     let botonReiniciar = document.getElementById('boton-Reiniciar')
